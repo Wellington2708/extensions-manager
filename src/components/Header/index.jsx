cd90e552538d95b87e85e6a03fc1b567c640cbfa
Wellington Junior <wellingtonjunior1027@gmail.com>
2026-02-18 21:18:13 +0000

    update image directory

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,13 @@
-import styles from './Header.module.css'
+import styles from './Header.module.css';
+import logoSvg from '../../assets/images/logo.svg';
+import iconMoon from '../../assets/images/icon-moon.svg';
+
 
 function Header(){
     return (
         <header className={styles.header}>
-            <img src="src/assets/images/logo.svg" alt="logo Extensions" className={styles.header_logo}/>
-            <button className={styles.btn_dark}><img src="src/assets/images/icon-moon.svg" alt="logo Extensions" className={styles.button_image_dark}/></button>
+            <img src={logoSvg} alt="logo Extensions" className={styles.header_logo}/>
+            <button className={styles.btn_dark}><img src={iconMoon} alt="logo Extensions" className={styles.button_image_dark}/></button>
         </header>
     )
 
